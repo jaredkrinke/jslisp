944be9afd43e70678a67706d929e0483bfc9d3ce
jaredkrinke <jaredkrinke@gmail.com>
2014-11-22 21:08:40 -0800

    Tokenizer

--- main.js
+++ main.js
@@ -1,8 +1,65 @@
 ﻿$(function () {
+    var tokenize = function (input) {
+        var output = [];
+        var inToken = false;
+        var tokenStart = 0;
+        for (var i = 0, count = input.length; i < count; i++) {
+            if (inToken) {
+                switch (input[i]) {
+                    case '(':
+                    case ')':
+                        output.push(input.slice(tokenStart, i));
+                        output.push(input[i]);
+                        inToken = false;
+                        break;
+
+                    case ' ':
+                        output.push(input.slice(tokenStart, i));
+                        inToken = false;
+                        break;
+                }
+            } else {
+                switch (input[i]) {
+                    case '(':
+                    case ')':
+                        output.push(input[i]);
+                        break;
+
+                    case ' ':
+                        break;
+
+                    default:
+                        inToken = true;
+                        tokenStart = i;
+                        break;
+                }
+            }
+        }
+
+        if (inToken) {
+            output.push(input.slice(tokenStart));
+        }
+
+        return output;
+    };
+
+    var parse = function (input) {
+    };
+
+    var process = function (input) {
+        var output = '';
+        var tokens = tokenize(input);
+        for (var i = 0, count = tokens.length; i < count; i++) {
+            output += 'Token: ' + tokens[i] + '\n';
+        }
+
+        return output;
+    };
+
     var input = $('#input');
     var output = $('#output');
     $('#execute').click(function (e) {
         e.preventDefault();
-        output.text(input.text());
+        output.text(process(input.text()));
     });
 });
