468b5c32545424320896ccf37a8ca3f97678784e
jaredkrinke <jaredkrinke@gmail.com>
2014-11-22 20:42:40 -0800

    Skeleton

---
+++ main.js
@@ -0,0 +1,8 @@
+﻿$(function () {
+    var input = $('#input');
+    var output = $('#output');
+    $('#execute').click(function (e) {
+        e.preventDefault();
+        output.text(input.text());
+    });
+});
