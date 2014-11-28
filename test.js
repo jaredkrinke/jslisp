4021fd87e78f4229215a04c705e644eae7f7a6f9
jaredkrinke <jaredkrinke@gmail.com>
2014-11-28 09:08:19 -0800

    More tests

--- test.js
+++ test.js
@@ -28,14 +28,61 @@ test('1.1.1', [
 ]);
 
 test('1.1.2', [
-    ['(define size 2)', undefined],
+    ['(define size 2)'],
     ['size', 2],
     ['(* 5 size)', 10],
-    ['(define pi 3.14159)', undefined],
-    ['(define radius 10)', undefined],
+    ['(define pi 3.14159)'],
+    ['(define radius 10)'],
     ['(* pi (* radius radius))', 314.159],
-    ['(define circumference (* 2 pi radius))', undefined],
+    ['(define circumference (* 2 pi radius))'],
     ['circumference', 62.8318],
+]);
+
+test('1.1.3', [
+    ['(* (+ 2 (* 4 6)) (+ 3 5 7))', 390],
+]);
+
+test('1.1.4', [
+    ['(define (square x) (* x x))'],
+    ['(square 21)', 441],
+    ['(square (+ 2 5))', 49],
+    ['(square (square 3))', 81],
+    ['(define (sum-of-squares x y) (+ (square x) (square y)))'],
+    ['(sum-of-squares 3 4)', 25],
+    ['(define (f a) (sum-of-squares (+ a 1) (* a 2)))'],
+    ['(f 5)', 136],
+]);
+
+test('1.1.6', [
+    ['(define (abs x) (cond ((> x 0) x) ((= x 0) 0) ((< x 0) (- x))))'],
+    ['(define (abs x) (cond ((< x 0) (- x)) (else x)))'],
+    ['(define (abs x) (if (< x 0) (- x) x))'],
+    ['(and (> x 5) (< x 10))', false],
+    ['(define (>= x y) (or (> x y) (= x y)))'],
+    ['(define (>= x y) (not (< x y)))'],
+]);
+
+test('1.1.7', [
+    ['(define (abs x) (if (< x 0) (- x) x))'],
+    ['(define (square x) (* x x))'],
+    ['(define (sqrt-iter guess x) (if (good-enough? guess x) guess (sqrt-iter (improve guess x) x)))'],
+    ['(define (improve guess x) (average guess (/ x guess)))'],
+    ['(define (average x y) (/ (+ x y) 2))'],
+    ['(define (good-enough? guess x) (< (abs (- (square guess) x)) 0.001))'],
+    ['(define (sqrt x) (sqrt-iter 1.0 x))'],
+    ['(sqrt 9)', 3.00009155413138],
+    ['(sqrt (+ 100 37))', 11.704699917758145],
+    ['(sqrt (+ (sqrt 2) (sqrt 3)))', 1.7739279023207892],
+    ['(square (sqrt 1000))', 1000.000369924366],
+]);
+
+test('1.1.8', [
+    ['(define (abs x) (if (< x 0) (- x) x))'],
+    ['(define (square x) (* x x))'],
+    ['(define (average x y) (/ (+ x y) 2))'],
+    ['(define (sqrt x) (define (good-enough? guess x) (< (abs (- (square guess) x)) 0.001)) (define (improve guess x) (average guess (/ x guess))) (define (sqrt-iter guess x) (if (good-enough? guess x) guess (sqrt-iter (improve guess x) x))) (sqrt-iter 1.0 x))'],
+    ['(define (sqrt x) (define (good-enough? guess) (< (abs (- (square guess) x)) 0.001)) (define (improve guess) (average guess (/ x guess))) (define (sqrt-iter guess) (if (good-enough? guess) guess (sqrt-iter (improve guess)))) (sqrt-iter 1.0))'],
+    ['(sqrt 9)', 3.00009155413138],
 ]);
 
 //test('', [
